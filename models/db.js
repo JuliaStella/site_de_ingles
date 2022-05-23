47531ef55e4b968c03f2896bb3fecf5eb1a48d17
Júlia Stella <juliastellasil1@gmail.com>
2022-05-23 16:59:34 -0300

    Integração de node.js e mysql

---
+++ models/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('site', 'root' ,'root',{
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexão com o banco de dados realizada com sucesso!");
+}).catch(function(){
+  console.log("Erro: conexão com o banco de dados não realizada");
+});
+
+module.exports = sequelize;
